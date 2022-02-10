bd57c7e4d85904301ea74207440a7e686ee03125
Diego Caucota <diegocaucota@gmail.com>
2022-02-10 00:27:04 -0300

    Componentes Header y Formulario

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
 
+import styled from '@emotion/styled';
+
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+ background-color: #FFF;
+ padding: 3rem;
+`;
 
 function App() {
   return (
-    <h1>Cotizador</h1>
+    <Contenedor>
+      <Header 
+        titulo='Cotizador de Seguros'
+      />
+
+      <ContenedorFormulario>
+        <Formulario />   
+      </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
